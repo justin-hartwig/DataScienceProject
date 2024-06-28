6f56b1d2dcf45c06d6a0bff3c104652bf6927402
Justin Hartwig <65766832+justin-hartwig@users.noreply.github.com>
2024-06-28 13:23:39 +0200

    Optimized ranking info icon display in app

--- js/openStreetMapLeaflet.js
+++ js/openStreetMapLeaflet.js
@@ -85,7 +85,7 @@ function onEachFeature(feature, layer) {
             let priceRating = "";
             let infoIcon = "";
             if(county.anomalieErrorType) {
-                infoIcon = `<a href="/#anomalies" title="Wie kommen wir auf diese Einschätzung?"><i class="fa-solid fa-circle-info"></i></a>`;
+                infoIcon = `<a href="/#anomalies" title="Wie kommen wir auf diese Einschätzung?"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="fa-info-icon"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM216 336h24V272H216c-13.3 0-24-10.7-24-24s10.7-24 24-24h48c13.3 0 24 10.7 24 24v88h8c13.3 0 24 10.7 24 24s-10.7 24-24 24H216c-13.3 0-24-10.7-24-24s10.7-24 24-24zm40-208a32 32 0 1 1 0 64 32 32 0 1 1 0-64z"/></svg></i></a>`;
             }
             if(county.anomalieErrorType == "Correct") {
                 priceRating = `<span class="text-highlight">(Fairer Preis)</span>`;
