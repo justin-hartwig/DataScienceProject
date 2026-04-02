b5da1a5a81dcd2dcaeb0ee66137b63c522fbe3c3
Justin Hartwig <65766832+justin-hartwig@users.noreply.github.com>
2026-04-02 18:39:03 +0200

    Changed text for question 2

--- js/quiz.js
+++ js/quiz.js
@@ -15,7 +15,7 @@ let confettiElement;
 
 export function initializeQuiz() {
     question1 = new QuizQuestion("quiz-question-1", "quizQuestion1Answer2", "Richtige Antwort! Das durchschnittliche verfügbare Einkommen pro Kopf in Deutschland im Jahr 2021 beträgt 23.667€.", "disposable-income-mean", disposableIncomeMean);
-    question2 = new QuizQuestion("quiz-question-2", "quizQuestion2Answer2", "Richtige Antwort! Text fehlt noch!", "rental-prices-states-median", rentalPricesStatesMedian);
+    question2 = new QuizQuestion("quiz-question-2", "quizQuestion2Answer2", "Richtige Antwort! Der Median der Meitpreise in Deutschland hat 2022 9.66 € pro m² betragen.", "rental-prices-states-median", rentalPricesStatesMedian);
     question3 = new QuizQuestion("quiz-question-3", "quizQuestion3Answer1", `Richtige Antwort! Es besteht ein Zusammenhang zwischen dem Altersdurchschnitt und der Bevölkerungsdichte. In dem Scatterplot ist deutlich eine negative Korrelation mit dem Wert -0,68 zwischen den Variablen zu erkennen.
 Dies bedeutet, dass mit zunehmendem Durchschnittsalter die Bevölkerungsdichte tendenziell abnimmt.
 `, "rental-prices-states-median", correlationAgeAveragePopulationDesity);
